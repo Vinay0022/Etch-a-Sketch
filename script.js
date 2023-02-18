a3a69cc1036a8de03c60fa939e2b8c3f97342d2e
Vinay <vinu031995@gmail.com>
2023-02-18 20:05:25 +0530

    fixed the Squared getting out of container

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let Squares;
 const btn = document.querySelector('button');
 
 btn.addEventListener('click',()=>{
-    
 let grid= prompt('Enter the grid :' )
 let num = parseInt(grid);
-     
+    let swidth = 600/num-2;
+    let sheight= 600/num-2;
 if(num>99){ 
     alert("Please Enter No. less than 100"); 
 return ;
@@ -15,6 +15,8 @@ return ;
 for (let index = 0; index < num*num; index++) {
 Squares = document.createElement('div');
    Squares.classList.add('squares'); 
+    console.log(swidth);
+  Squares.style=`width:${swidth}px; height:${sheight}px;`; 
 container.appendChild(Squares);
 }
 
