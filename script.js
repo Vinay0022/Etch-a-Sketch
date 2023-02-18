da4a43b53671a2db75cdfc7346dc16af28cdd6a1
Vinay <vinu031995@gmail.com>
2023-02-18 20:15:03 +0530

    fixed grid adding after each grid selection

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ const container = document.querySelector('.container');
 let Squares;
 const btn = document.querySelector('button');
 
+for (let index = 0; index < 16*16; index++) {
+Squares = document.createElement('div');
+   Squares.classList.add('squares'); 
+  Squares.style='width:35.5px; height:35.5px;'; 
+container.appendChild(Squares);
+}
+
 btn.addEventListener('click',()=>{
+                    
 let grid= prompt('Enter the grid :' )
 let num = parseInt(grid);
     let swidth = 600/num-2;
@@ -11,6 +19,9 @@ if(num>99){
     alert("Please Enter No. less than 100"); 
 return ;
 }
+
+ while (container.hasChildNodes())
+               container.removeChild(container.firstChild)
 
 for (let index = 0; index < num*num; index++) {
 Squares = document.createElement('div');
